5b2eefdc6f795dd210a1b8a04a79e4045bc4accb
Kyuhyun Kim <kyuhyun.kim@mytrum.com>
2019-02-28 13:37:40 +0900

    img col

--- App.js
+++ App.js
@@ -23,8 +23,11 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content"></StatusBar>
-        <Image style={{ width:'100%',resizeMode:'contain'}}source={require('./img/loginpage/bitmap2x.png')} />
-        <Image style={{ width:'100%',resizeMode:'contain'}}source={require('./img/loginpage/logo2x.png')} />
+        <Image style={{}}source={require('./img/loginpage/bitmap2x.png')} />
+        <Image style={{}}source={require('./img/loginpage/logo2x.png')} />
+        <Image style={imgStyle.facebookJoinBtn}source={require('./img/loginpage/facebook2x.png')} />
+        <Image style={imgStyle.naverJoinBtn}source={require('./img/loginpage/naver2x.png')} />
+        <Image style={imgStyle.kakaoJoinBtn}source={require('./img/loginpage/kakao2x.png')} />
         <Text style={styles.instructions}>{instructions}</Text>
         <Text style={styles.joinEmail}>{joinEmail}</Text>
       </View>
@@ -32,23 +35,32 @@ export default class App extends Component {
   }
 }
 
+const imgStyle = StyleSheet.create({
+  facebookJoinBtn: {
+     
+  },
+  naverJoinBtn :{
+     
+  },
+  kakaoJoinBtn:{
+    
+  }
+
+})
 
 const styles = StyleSheet.create({
+
   joinEmail: {
       color : "grey",
-      textAlign: "center",
-      marginTop : 40
+      textAlign: "center"
   },
   title: {
     color : "black",
     fontSize : 30,
-    marginTop : 10,
-    fontWeight : "200",
-    marginBottom : 0
+    fontWeight : "200"
 
   },
   container: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
@@ -56,12 +68,10 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 0,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 40,
   },
 
 });
