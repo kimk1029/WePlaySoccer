72b21d286b20deac025368d6789f1bb191b63d3e
Kyuhyun Kim <kyuhyun.kim@mytrum.com>
2019-03-22 18:07:05 +0900

    delete

--- android/app/src/main/java/com/weplaysoccer/MainApplication.java
+++ android/app/src/main/java/com/weplaysoccer/MainApplication.java
@@ -46,20 +46,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    try {
-  PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-  for (Signature signature : info.signatures) {
-    MessageDigest md = MessageDigest.getInstance("SHA");
-    md.update(signature.toByteArray());
-    Log.d("KeyHash:::::::::", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-  }
-} catch (PackageManager.NameNotFoundException e) {
-  e.printStackTrace();
-} catch (NoSuchAlgorithmException e) {
-  e.printStackTrace();
-}
-
-
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
